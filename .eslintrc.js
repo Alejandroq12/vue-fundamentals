816f52820194d25b8e222f6611b99a0c8cd03645
alejandroq12 <jaquezada@innovacion.gob.sv>
2023-08-15 14:54:11 -0600

    Erase prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,6 @@ module.exports = {
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "plugin:prettier/recommended",
   ],
   parserOptions: {
     parser: "@babel/eslint-parser",
