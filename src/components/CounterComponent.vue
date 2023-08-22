7b21242f90b296b831a6afbe8b5d3311de111dc1
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-08-21 19:34:41 -0600

    Agregar estilos a los botones

--- src/components/CounterComponent.vue
+++ src/components/CounterComponent.vue
@@ -33,7 +33,12 @@ export default {
 button {
   background-color: #64bb87;
   border-radius: 5px;
+  border: 1px solid white;
   margin: 0 5px;
   color: white;
+  cursor: pointer;
+  margin: 0 5px;
+  padding: 5px 15px;
+  transition: 0.3s ease-in-out;
 }
 </style>
