e6a7938c20095b2ab9ee1c58096a81e61d0509cf
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-08-30 20:29:49 -0600

    Add data binding

--- src/components/IndecisionComponent.vue
+++ src/components/IndecisionComponent.vue
@@ -1,14 +1,13 @@
 <template>
-  <h1>Indecision</h1>
-  <img src="https://via.placeholder.com/250" alt="background" />
-  <div class="bg-dark"></div>
-  <div class="indecision-container"></div>
-  <input type="text" placeholder="Hazme una pregunta" />
-  <p>Recuerda terminar con un signo de interrogacion (?)</p>
-
-  <div>
-    <h2>Seré millonario?</h2>
-    <h1>Si, No, ... pensando</h1>
+  <img src="https://via.placeholder.com/250" alt="bg">
+  <div class="bg-dark"></div>  
+  <div class="indecision-container">
+      <input type="text" placeholder="Hazme una pregunta">
+      <p>Recuerda terminar con un signo de interrogacion (?)</p>
+      <div>
+          <h2>Sere millonario?</h2>
+          <h1>Si, No, .... pensando</h1>
+      </div>
   </div>
 </template>
 
