84d595b4b65fb041320d5619f4ab8a557e1769e2
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-08-30 20:19:45 -0600

    Add styles

--- src/components/IndecisionComponent.vue
+++ src/components/IndecisionComponent.vue
@@ -16,4 +16,49 @@
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+
+    img, .bg-dark {
+        height: 100vh;
+        left: 0px;
+        max-height: 100%;
+        max-width: 100%;
+        position: fixed;
+        top: 0px;
+        width: 100vw;
+    }
+
+    .bg-dark {
+        background-color: rgba(0, 0, 0, 0.4);
+    }
+
+    .indecision-container {
+        position: relative;
+        z-index: 99;
+    }
+    
+    input {
+        width: 250px;
+        padding: 10px 15px;
+        border-radius: 5px;
+        border: none;
+    }
+    input:focus {
+        outline: none;
+    }
+
+    p {
+        color: white;
+        font-size: 20px;
+        margin-top: 0px;
+    }
+
+    h1, h2 {
+        color: white;
+    }
+    
+    h2 {
+        margin-top: 150px;
+    }
+
+</style>
