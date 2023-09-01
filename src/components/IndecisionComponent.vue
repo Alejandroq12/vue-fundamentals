53d47c7830275a73fd45e705408f1d6ee4daf091
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-09-01 13:13:13 -0600

    Properly indent code

--- src/components/IndecisionComponent.vue
+++ src/components/IndecisionComponent.vue
@@ -1,13 +1,13 @@
 <template>
-  <img src="https://via.placeholder.com/250" alt="bg">
-  <div class="bg-dark"></div>  
+  <img src="https://via.placeholder.com/250" alt="bg" />
+  <div class="bg-dark"></div>
   <div class="indecision-container">
-      <input type="text" placeholder="Hazme una pregunta" v-model="question">
-      <p>Recuerda terminar con un signo de interrogacion (?)</p>
-      <div>
-          <h2 :question="question">{{ question }}</h2>
-          <h1>Si, No, .... pensando</h1>
-      </div>
+    <input type="text" placeholder="Hazme una pregunta" v-model="question" />
+    <p>Recuerda terminar con un signo de interrogacion (?)</p>
+    <div>
+      <h2 :question="question">{{ question }}</h2>
+      <h1>Si, No, .... pensando</h1>
+    </div>
   </div>
 </template>
 
@@ -15,53 +15,53 @@
 export default {
   data: () => ({
     question: '',
-  })
+  }),
 };
 </script>
 
 <style scoped>
+img,
+.bg-dark {
+  height: 100vh;
+  left: 0px;
+  max-height: 100%;
+  max-width: 100%;
+  position: fixed;
+  top: 0px;
+  width: 100vw;
+}
 
-    img, .bg-dark {
-        height: 100vh;
-        left: 0px;
-        max-height: 100%;
-        max-width: 100%;
-        position: fixed;
-        top: 0px;
-        width: 100vw;
-    }
+.bg-dark {
+  background-color: rgba(0, 0, 0, 0.4);
+}
 
-    .bg-dark {
-        background-color: rgba(0, 0, 0, 0.4);
-    }
+.indecision-container {
+  position: relative;
+  z-index: 99;
+}
 
-    .indecision-container {
-        position: relative;
-        z-index: 99;
-    }
-    
-    input {
-        width: 250px;
-        padding: 10px 15px;
-        border-radius: 5px;
-        border: none;
-    }
-    input:focus {
-        outline: none;
-    }
+input {
+  width: 250px;
+  padding: 10px 15px;
+  border-radius: 5px;
+  border: none;
+}
+input:focus {
+  outline: none;
+}
 
-    p {
-        color: white;
-        font-size: 20px;
-        margin-top: 0px;
-    }
+p {
+  color: white;
+  font-size: 20px;
+  margin-top: 0px;
+}
 
-    h1, h2 {
-        color: white;
-    }
-    
-    h2 {
-        margin-top: 150px;
-    }
+h1,
+h2 {
+  color: white;
+}
 
+h2 {
+  margin-top: 150px;
+}
 </style>
