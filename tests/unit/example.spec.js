f7afbcf42dd61afc323d4fe96dbb4800a47122d8
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-09-10 20:19:42 -0600

    Add first test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,1 +1,17 @@
-describe()
+describe('Example component', () => {
+  test('Debe de ser mayor a 10', () => {
+
+    // Arange
+    let value = 5;
+
+    // Act
+    value = value + 2;
+
+    // Assert
+    if(value > 10 ) {
+      // TODO everything okay
+    } else {
+      throw `${ value } no es mayor a 10`
+    }
+  });
+});
