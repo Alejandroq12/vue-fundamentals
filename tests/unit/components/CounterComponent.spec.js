c634c1ac1d74f9e3df69db411b54aa06f3737fd2
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-09-11 21:25:29 -0600

    Use shallow mount

--- tests/unit/components/CounterComponent.spec.js
+++ tests/unit/components/CounterComponent.spec.js
@@ -3,6 +3,7 @@ import CounterComponent from '../../../src/components/CounterComponent.vue'
 
 describe('Counter Component', () => {
     test('debe de hacer match con el snapshot', () => {
+        const wrapper = shallowMount(CounterComponent)
         expect().toMatchSnapshot()
     })
 })
