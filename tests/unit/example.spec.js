2835ebcd73f49b47764a031ef6e206bafafdbaee
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-09-11 20:47:50 -0600

    Get started with testing

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -8,10 +8,6 @@ describe('Example component', () => {
     value = value + 2;
 
     // Assert
-    if(value > 10 ) {
-      // TODO everything okay
-    } else {
-      throw `${ value } no es mayor a 10`
-    }
+    
   });
 });
