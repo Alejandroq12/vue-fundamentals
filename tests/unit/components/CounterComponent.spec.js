ecf3afe09d671119228359b233ea91681a83cac8
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-09-11 21:23:41 -0600

    Prepare to test Counter component

--- tests/unit/components/CounterComponent.spec.js
+++ tests/unit/components/CounterComponent.spec.js
@@ -1,5 +1,8 @@
+import { shallowMount, mount } from '@vue/test-utils'
+import CounterComponent from '../../../src/components/CounterComponent.vue'
+
 describe('Counter Component', () => {
     test('debe de hacer match con el snapshot', () => {
-
+        expect().toMatchSnapshot()
     })
 })
