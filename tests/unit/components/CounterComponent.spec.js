8c228d6cb908543cefe9f16115563ae44a355f0f
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-09-12 21:12:06 -0600

    Test h2 element

--- tests/unit/components/CounterComponent.spec.js
+++ tests/unit/components/CounterComponent.spec.js
@@ -1,9 +1,14 @@
-import { shallowMount, mount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import CounterComponent from '../../../src/components/CounterComponent.vue'
 
 describe('Counter Component', () => {
-    test('debe de hacer match con el snapshot', () => {
+    // test('debe de hacer match con el snapshot', () => {
+    //     const wrapper = shallowMount(CounterComponent)
+    //     expect(wrapper.html()).toMatchSnapshot()
+    // })
+    test('h2 debe de tener el valor por defecto.', () => {
         const wrapper = shallowMount(CounterComponent)
-        expect(wrapper.html()).toMatchSnapshot()
+        const h2 = wrapper.find('h2')
+        console.log(h2.text())
     })
 })
