010cd59215ff07d589990c96173dc8dc833199dc
alejandroq12 <jaquezada@innovacion.gob.sv>
2023-09-14 13:00:17 -0600

    Add snapshot

--- tests/unit/components/IndecisionComponent.spec.js
+++ tests/unit/components/IndecisionComponent.spec.js
@@ -1,2 +1,13 @@
-import { shallowMount } from "@vue/test-utils";
-import IndecisionComponent from "../../../src/components/IndecisionComponent.vue";
+import { shallowMount } from '@vue/test-utils';
+import IndecisionComponent from '../../../src/components/IndecisionComponent.vue';
+
+describe('IndecisionComponent', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(IndecisionComponent);
+  });
+  test('Debe de hacer match con el snapshot', () => {
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
