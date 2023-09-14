23dd56189d0b8107f9d1706e985e17d573892a39
alejandroq12 <jaquezada@innovacion.gob.sv>
2023-09-14 10:27:41 -0600

    Add beforeEach hook

--- tests/unit/components/CounterComponent.spec.js
+++ tests/unit/components/CounterComponent.spec.js
@@ -2,25 +2,31 @@ import { shallowMount } from '@vue/test-utils';
 import CounterComponent from '../../../src/components/CounterComponent.vue';
 
 describe('Counter Component', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(CounterComponent);
+
+  })
   // test('debe de hacer match con el snapshot', () => {
   //     const wrapper = shallowMount(CounterComponent)
   //     expect(wrapper.html()).toMatchSnapshot()
   // })
   test('h2 debe de tener el valor por defecto.', () => {
-    const wrapper = shallowMount(CounterComponent);
     expect(wrapper.find('h2').exists()).toBeTruthy();
     const h2Value = wrapper.find('h2').text();
     expect(h2Value).toBe('Counter');
   });
 
-  test('Default value must be 100', () => {
-    const wrapper = shallowMount(CounterComponent);
+  test('Default value must be 100', async () => {
     const value = wrapper.find('[data-testid="counter"]').text();
     expect(value).toBe('100');
+
+    const [increaseBtn, decreaseBtn ] = wrapper.findAll('button');
+    await increaseBtn.trigger('click');
   });
 
   test('It must be increase and decrease the value of the counter', async () => {
-    const wrapper = shallowMount(CounterComponent);
     const [increaseBtn, decreaseBtn] = wrapper.findAll('button');
 
     await increaseBtn.trigger('click');
