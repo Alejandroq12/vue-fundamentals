e1e701ccc2b685a0d57fdfb5fc827e5e641d2c9a
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-09-16 20:28:38 -0600

    Improve code indentation and readability

--- tests/unit/components/IndecisionComponent.spec.js
+++ tests/unit/components/IndecisionComponent.spec.js
@@ -5,13 +5,16 @@ describe('IndecisionComponent', () => {
   let wrapper;
   let clgSpy;
 
-  global.fetch = jest.fn( () => Promise.resolve({
-    json: () => Promise.resolve({
-      'answer': 'yes',
-      'forced': false,
-      'image': 'https://yesno.wtf/assets/yes/2.gif'
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      json: () =>
+        Promise.resolve({
+          answer: 'yes',
+          forced: false,
+          image: 'https://yesno.wtf/assets/yes/2.gif',
+        }),
     })
-  }));
+  );
 
   beforeEach(() => {
     wrapper = shallowMount(IndecisionComponent);
@@ -25,40 +28,38 @@ describe('IndecisionComponent', () => {
   });
 
   test('Should not dispatch when typing on input console.log()', async () => {
-    const getAnswerSpy = jest.spyOn(wrapper.vm, 'getAnswer')
+    const getAnswerSpy = jest.spyOn(wrapper.vm, 'getAnswer');
     const input = wrapper.find('input');
     await input.setValue('Hola mundo');
 
     expect(clgSpy).toHaveBeenCalledTimes(1);
     expect(getAnswerSpy).not.toHaveBeenCalled();
-
   });
 
   test('Question mark "?" must dispatch getAnswer', async () => {
-    const getAnswerSpy = jest.spyOn(wrapper.vm, 'getAnswer')
+    const getAnswerSpy = jest.spyOn(wrapper.vm, 'getAnswer');
     const input = wrapper.find('input');
     await input.setValue('?');
 
     expect(clgSpy).toHaveBeenCalledTimes(1);
     expect(getAnswerSpy).toBeCalled();
-
-  })
+  });
 
   test('getAnswer tests', async () => {
-   await wrapper.vm.getAnswer()
+    await wrapper.vm.getAnswer();
 
-   const img = wrapper.find('img')
+    const img = wrapper.find('img');
 
-   expect(img.exists()).toBeTruthy()
-   expect(wrapper.vm.img).toBe('https://yesno.wtf/assets/yes/2.gif')
-   expect(wrapper.vm.answer).toBe('Si')
-  })
+    expect(img.exists()).toBeTruthy();
+    expect(wrapper.vm.img).toBe('https://yesno.wtf/assets/yes/2.gif');
+    expect(wrapper.vm.answer).toBe('Si');
+  });
 
   test('getAnswer test - API failure', async () => {
-    fetch.mockImplementationOnce(() => Promise.reject('API is down!'))
-    await wrapper.vm.getAnswer()
-    const img = wrapper.find('img')
-    expect(img.exists()).toBeFalsy()
-    expect(wrapper.vm.answer).toBe('No se pudo cargar el API')
-  })
+    fetch.mockImplementationOnce(() => Promise.reject('API is down!'));
+    await wrapper.vm.getAnswer();
+    const img = wrapper.find('img');
+    expect(img.exists()).toBeFalsy();
+    expect(wrapper.vm.answer).toBe('No se pudo cargar el API');
+  });
 });
